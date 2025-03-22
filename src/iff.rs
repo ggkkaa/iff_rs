70dcef4343a801bb5d54b3e383f66134d486196d
Luka <luka.talevski.hn@gmail.com>
2025-03-22 06:43:05 +0100

    feat(read): Read IFF files

--- src/iff.rs
+++ src/iff.rs
@@ -1,6 +1,6 @@
-use std::io::{self, Read};
 use crate::chunk::IFFChunk;
 use byteorder::{BigEndian, ReadBytesExt};
+use std::io::{self, Read};
 
 pub struct IFFFile {
     pub chunks: Vec<IFFChunk>,
@@ -8,9 +8,7 @@ pub struct IFFFile {
 
 impl IFFFile {
     pub fn new() -> IFFFile {
-        IFFFile {
-            chunks: Vec::new(),
-        }
+        IFFFile { chunks: Vec::new() }
     }
 
     pub fn add_chunk(&mut self, chunk: IFFChunk) {
@@ -19,14 +17,18 @@ impl IFFFile {
 }
 
 pub fn process_iff<R: Read>(reader: R) -> Result<IFFFile, io::Error> {
-        let mut iff_file = IFFFile::new();
-        let mut reader = io::BufReader::new(reader);
+    let mut iff_file = IFFFile::new();
+    let mut reader = io::BufReader::new(reader);
 
-        while let Ok(chunk) = parse_chunk(&mut reader) {
-            iff_file.add_chunk(chunk);
-        }
+    let mut header = [0u8; 4];
+    reader.read_exact(&mut header)?;
+    let _ = reader.read_u32::<BigEndian>()?;
 
-        Ok(iff_file)
+    while let Ok(chunk) = parse_chunk(&mut reader) {
+        iff_file.add_chunk(chunk);
+    }
+
+    Ok(iff_file)
 }
 
 fn parse_chunk<R: Read>(reader: &mut R) -> Result<IFFChunk, io::Error> {
