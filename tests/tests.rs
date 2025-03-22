853bec71f75ffc66308812c22995e9b4f8000570
Luka <luka.talevski.hn@gmail.com>
2025-03-22 11:24:06 +0100

    Add dual chunk test

--- tests/tests.rs
+++ tests/tests.rs
@@ -12,6 +12,19 @@ fn create_sample_iff() -> Result<(), Error> {
     Ok(())
 }
 
+fn create_dual_sample_iff() -> Result<(), Error> {
+    let mut file = File::create("test-files/dual_sample.iff")?;
+    file.write_all(b"FORM")?;
+    file.write_all(&[0, 0, 0, 8])?;
+    file.write_all(b"DATA")?;
+    file.write_all(&[0, 0, 0, 4])?;
+    file.write_all(b"abcd")?;
+    file.write_all(b"DATA")?;
+    file.write_all(&[0, 0, 0, 4])?;
+    file.write_all(b"efgh")?;
+    Ok(())
+}
+
 fn create_empty_iff() -> Result<(), Error> {
     let _file = File::create("test-files/empty.iff")?;
     Ok(())
@@ -40,4 +53,23 @@ fn test_parse_empty_iff() {
     let iff = parse_iff(file);
 
     assert!(iff.is_err(), "Empty IFF file should return an error");
+}
+
+#[test]
+fn test_parse_dual_iff() {
+    create_dual_sample_iff().expect("Failed to create sample IFF file");
+
+    let file: File = File::open("test-files/dual_sample.iff").expect("Failed to open sample IFF file");
+    let iff = parse_iff(file).expect("Failed to parse IFF file");
+
+    assert_eq!(iff.chunks.len(), 2);
+    let mut chunk = &iff.chunks[0];
+    assert_eq!(chunk.id, u32::from_be_bytes(*b"DATA"));
+    assert_eq!(chunk.len, 4);
+    assert_eq!(chunk.data, b"abcd");
+    
+    chunk = &iff.chunks[1];
+    assert_eq!(chunk.id, u32::from_be_bytes(*b"DATA"));
+    assert_eq!(chunk.len, 4);
+    assert_eq!(chunk.data, b"efgh");
 }
